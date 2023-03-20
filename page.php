c1bf7ccf31a9cbd2fcdf146665e121d756951ef0
Arif Rahman <53683822+Rony015@users.noreply.github.com>
2023-03-20 19:08:40 +0600

    some css

--- page.php
+++ page.php
@@ -35,22 +35,23 @@ $page_sidebar = singleland_opt('page_layout');
 		  * hook --_singleland_banner -- 10
 		  */
 		 do_action('singleland_banner_content', $banner_id, 'page');
-
-	    singleland_wrapper_start( $page_sidebar  );
-
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		singleland_wrapper_end( $page_sidebar );
 		?>
+		<div class="container sec_padding">
+			<?php 
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			
+			?>
+		</div>
 	</main><!-- #main -->
 
 <?php
