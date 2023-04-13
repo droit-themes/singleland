d20845d98c6a5c3f50a2cd6aea0a0b40d3df126c
Abdur Rahaman <79562059+dlab10@users.noreply.github.com>
2023-04-13 10:03:01 +0600

    updated

--- inc/plugin_activation.php
+++ inc/plugin_activation.php
@@ -39,13 +39,13 @@ function singleland_resister_required_plugin() {
         array(
             'name'               => esc_html__( 'Singleland Core', 'singleland' ), // The plugin name.
             'slug'               => 'singleland-core', // The plugin slug (typically the folder name).
-            'source'             => 'https://singleland.droitlab.com/downloadfile/singleland-core.zip', // The plugin source.
+            'source'             => 'https://singleland.droitlab.com/downloadfile/singleland-core_1.0.1.zip', // The plugin source.
             'required'           => true, // If false, the plugin is only 'recommended' instead of required.
             'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
             'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
             'external_url'       => '', // If set, overrides default API URL and points to an external URL.
             'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-            'version'            => '1.0.0'
+            'version'            => '1.0.1'
         ),
 
         array(
