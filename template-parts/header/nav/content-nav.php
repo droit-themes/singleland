bc326f2a50d8961c3885da00ebecf2faf2ca60c8
Omar Faruk <omarfaruk2y2@gmail.com>
2024-04-27 17:26:06 +0600

    update

--- template-parts/header/nav/content-nav.php
+++ template-parts/header/nav/content-nav.php
@@ -2,6 +2,13 @@
 $singleland_header_layout = singleland_opt('singleland_header_layout', 'container'); 
 $singleland_menu_layout = singleland_opt('singleland_menu_layout', 'center'); 
 
+$opt = get_option('singleland_opt');
+
+$display_search  = isset( $opt['is_menu_btn']) ? $opt['is_menu_btn'] : '';
+$menu_btn_label  = isset( $opt['menu_btn_label']) ? $opt['menu_btn_label'] : '';
+$menu_btn_url    = isset( $opt['menu_btn_url']) ? $opt['menu_btn_url'] : '';
+$is_target_blank    = isset( $opt['is_target_blank']) ? $opt['is_target_blank'] : '';
+
 ?>
 
 <nav class="navbar navbar-expand-lg menu_one">
@@ -22,6 +29,11 @@ $singleland_menu_layout = singleland_opt('singleland_menu_layout', 'center');
             ?>
             
         </div>
+
+            <div class="header_area nav_bar">
+            <a class="btn_get btn-meta btn_hover" <?php if($is_target_blank == 1): ?> target="_blank" <?php endif; ?> href="<?php echo esc_url($menu_btn_url); ?>"><?php echo ($menu_btn_label); ?></a>
+            </div>
+
         <?php get_template_part( 'template-parts/header/nav/content-nav', 'serch'); ?>
     </div>
 </nav>
